31608f76df48b8335f7d65f018ba20a14e016fd4
tea <tsvegor@gmail.com>
2025-01-29 21:00:15 +0300

    add script for generating basic set of tasks

---
+++ scripts/generate_basic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+RAM=$((4*1024)) # in Megabytes
+dims=""
+mIntervals=""
+nPointsPerCell=""
+for dim in {1..30}
+do
+	dims="${dims} -d${dim}"
+done
+for m in 2 10 20 50 100 1000 10000 100000 1000000
+do
+	mIntervals="${mIntervals} -m${m}"
+done
+for M in 10 100 1000 10000 100000 1000000
+do
+	nPointsPerCell="${nPointsPerCell} -M${m}"
+done
+
+echo "Dimensions: " $dims
+echo "mIntervalsPerDim " $mIntervals
+echo "nPointsPerCell " $nPointsPerCell
+./bin/hypercube generate $dims $mIntervals $nPointsPerCell --ram $RAM
+

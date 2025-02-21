0a114ef4e2ac35cc9aa6eb95873e00e7934b44d4
Egor Tsvetkov <e.tsvetkov@digital-core.ru>
2025-02-22 02:56:31 +0300

    add HYPERCUBE_PROJECT_ROOT to docker-compose.yaml

--- docker-compose-run.sh
+++ docker-compose-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#docker compose -f airflow/docker-compose.yaml build #--no-cache
-#docker compose -f airflow/docker-compose.yaml up airflow-init
-#docker compose -f airflow/docker-compose.yaml up
-docker compose build #--no-cache
-docker compose up airflow-init
-docker compose up
+docker compose -f airflow/docker-compose.yaml build #--no-cache
+docker compose -f airflow/docker-compose.yaml up airflow-init
+docker compose -f airflow/docker-compose.yaml up
+#docker compose build #--no-cache
+#docker compose up airflow-init
+#docker compose up
